cbf22eb5825080b3c1958bad2e85f38d2f95114f
yoppp11 <yopinur040903@gmail.com>
2023-06-16 20:10:14 +0630

    fix komuniti

--- app/src/main/java/com/example/develovmain/Ui/Komuniti.kt
+++ app/src/main/java/com/example/develovmain/Ui/Komuniti.kt
@@ -1,3 +1,13 @@
 package com.example.develovmain.Ui
 
-data class Komunitipage()
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+
+data class Komuniti(
+    val imgprofilKomuniti: Int,
+    val nameprofilKomuniti: String,
+    val judulpostKomuniti: String,
+    val descpostKomuniti: String
+)
--- app/src/main/java/com/example/develovmain/Ui/UploadBuktiActivity.kt
+++ app/src/main/java/com/example/develovmain/Ui/UploadBuktiActivity.kt
@@ -1,12 +1,46 @@
 package com.example.develovmain.Ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.develovmain.R
+import com.example.develovmain.databinding.ActivityUploadBuktiBinding
+import java.io.File
 
 class UploadBuktiActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBuktiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload_bukti)
+        binding = ActivityUploadBuktiBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnUp.setOnClickListener { showFileChooser() }
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Pilih file"), 100)
+        } catch (exception: Exception) {
+            Toast.makeText(this, "Please install a file manager.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            val uri: Uri? = data.data
+            val path: String = uri?.path.toString()
+            val file = File(path)
+            binding.txtResult.text = "Path: $path File name: ${file.name}".trimIndent()
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
