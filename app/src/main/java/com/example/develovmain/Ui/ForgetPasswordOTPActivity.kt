2ba325b41500914508a70393a996f485e6680153
unknown <sari.142002@gmail.com>
2023-06-21 12:01:18 +0700

    update page ForgetPassword, ForgetPasswordOTP, ChangePassword

---
+++ app/src/main/java/com/example/develovmain/Ui/ForgetPasswordOTPActivity.kt
@@ -0,0 +1,12 @@
+package com.example.develovmain.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.develovmain.R
+
+class ForgetPasswordOTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password_otp)
+    }
+}
